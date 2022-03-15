5d9f06d9a356ba4682f78fac62f541d9e2479e09
tahrimhossain <tahrimh246@gmail.com>
2022-03-15 23:49:39 +0600

    added firebase auth

--- app.py
+++ app.py
@@ -7,6 +7,9 @@ from bson import json_util
 import json
 import os
 import random
+import firebase_admin
+from firebase_admin import credentials,auth
+
 
 app = Flask(__name__)
 cors = CORS(app,resources = {r"*":{"origins":"*"}})
@@ -16,6 +19,9 @@ mongoURL = os.environ.get('MONGO_URL')
 
 client = MongoClient(mongoURL)
 database = client["lexisDatabse"]
+
+
+firebase_admin.initialize_app(credentials.Certificate(json.loads(os.environ.get('FIREBASE'))))
 
 class Words(Resource):
 	
@@ -33,19 +39,30 @@ class Words(Resource):
 
 class Categories(Resource):
 	
-	def get(self):
+	def get(self,uid):
 
-		data = database.categories.find_one({"_id":"categories"})
-		if data == None:
-			abort(404,message = "Data not found")
+		try:
+			user = auth.get_user(uid)
+			if database.playerbest.count_documents({'_id':uid},limit = 1) == 0:
+				database.playerbest.insert_one({'_id':uid,'Best_Scores':[{"Category_Id": "4",'Score':0},{"Category_Id": "5",'Score':0},{"Category_Id": "6",'Score':0},{"Category_Id": "7",'Score':0}]})
 
-		data.pop("_id")
-		return data		
+			bestScore = database.playerbest.find_one({'_id':uid})
+			categories = database.categories.find_one({'_id':'categories'})
+
+			for category in categories['Categories']:
+				for score in bestScore['Best_Scores']:
+					if score['Category_Id'] == category['Category_Id']:
+						category["Best_Score"] = score["Score"]
+						break
+			return categories
+						
+		except Exception as e:
+			abort(400,message = "Invalid uid")
 
 
 
 api.add_resource(Words,"/words/<string:numberOfLetters>/<int:numberOfWords>")
-api.add_resource(Categories,"/categories")
+api.add_resource(Categories,"/categories/<string:uid>")
 
 if __name__ == "__main__": 
 	app.run()	
